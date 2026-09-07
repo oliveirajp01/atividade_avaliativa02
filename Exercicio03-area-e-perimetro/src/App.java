a8c8356a763f1936be87c5e36727ebe400936f0c
João Pedro Dos Santos Oliveira Carneiro <joaopedrocarneiro01@gmail.com>
2026-09-07 16:37:10 +0000

    Exercicio03

---
+++ Exercicio03-area-e-perimetro/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner Leitor = new Scanner(System.in);
+
+        System.out.print("Base: ");
+        double base = Leitor.nextDouble();
+        System.out.print("Altura: ");
+        double altura = Leitor.nextDouble();
+        double area = base*altura;
+        System.out.println("Área: " + area);
+        double perimetro = 2*(base+altura);
+        System.out.println("Perímetro: " + perimetro);
+
+        Leitor.close();
+        
+
+
+    }
+}
